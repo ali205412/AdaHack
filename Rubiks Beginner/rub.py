aec39836315224cc3fdfee4063e7f319339bb866
aroch17 <86850534+aroch17@users.noreply.github.com>
2022-10-15 10:45:40 +0100

    Added rub3.py

--- Rubiks Beginner/rub.py
+++ Rubiks Beginner/rub.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time
 
 blue = 114, 187, 255
 white = 230, 230, 255
@@ -69,4 +70,3 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-# paint([[0, 0, 0, 0], [1, 1, 1,1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5]])
